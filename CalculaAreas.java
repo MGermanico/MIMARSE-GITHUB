822ebea956248855aca1dcff7f92d531c072776c
Miguel Garcia <miguel.garcia42@educa.madrid.orggit config --global user.name Miguel>
2024-01-12 16:39:58 +0100

    añadido pregunta inicial al usuario y Util.Scanner importado

---
+++ CalculaAreas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculaareas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @MiguelMarcosAntonio dev
+ */
+public class CalculaAreas {
+    public static void main(String[] args) {
+        menu();
+    }
+    public static void menu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n\t--->\tElige una figura geométrica:\n\n"
+                + "1.Triangulo\n"
+                + "2.Cuadrado\n"
+                + "3.(n)Ágono\n\n");
+    }
+}
