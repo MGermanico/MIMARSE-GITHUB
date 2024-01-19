a8459ebe16809eb74cbb72ef02d1bd54a11ddf0f
Antonio Aldea <felix.aldea@educa.madrid.org>
2024-01-19 16:15:50 +0100

    Cuadrado arreglado

--- CalculaAreas.java
+++ CalculaAreas.java
@@ -21,7 +21,7 @@ public class CalculaAreas {
                 + "2.Cuadrado\n"
                 + "3.(n)Ágono\n\n");
     }
-	public static void cuadrado(int n) {
+	public static void cuadrado(double n) {
 	System.out.println(n * n);
 	}
 }
